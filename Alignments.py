3cfa8b9df3d3102f6300f0aad37374f3ccc9245d
Israel Roman Godinez <israel.roman@cucei.udg.mx>
2016-09-27 19:17:15 -0500

    main.py: Se agregaron dos variables: 1) 'clustalw2_path' contiene la ruta y el nombre del programa clustalw2 2) sequences_file_path que contiene la ruta y el nombre del archivo donde se leeran las secuencias a ser alineadas
    Alignments.py: seagrego la variable 'clustal2_path' a la funcion 'clustal_align' para indicar la ruta del programa clustalw2
    
    Alignment.py: se requiere que el comando 'cline' lleve parentesis 'cline()' para que se ejecute el comando, de lo contrario no lo ejecuta

--- Alignments.py
+++ Alignments.py
@@ -5,12 +5,12 @@ from Bio import Phylo
 #from Bio.Align.Applications import MuscleCommandline
 from Bio.Align.Applications import ClustalwCommandline
 
-def clustal_align(input_file):
+def clustal_align(input_file, clustalw_path):
 	#clustalw_exe = "/home/karla/Desktop/ejemplos_biopython/biopython\clustalw2.exe"
 	#clustalw_cline = ClustalwCommandline(clustalw_exe, infile=input_file)
-	assert os.path.isfile("clustalw2"), "Clustal W executable missing"
-	cline = ClustalwCommandline("clustalw2", infile=input_file)
-	print(cline)
+	assert os.path.isfile(path), "Clustal W executable missing"
+	cline = ClustalwCommandline(path, infile=input_file)
+	cline() # Este comando ejecuta el programa de clustalw con los parametros que fueron establecidos. Son necesarios los parentesis para que se ejecute el proceso
 	
 def show_align(input_file):
 	align = AlignIO.read(input_file, "clustal")
