bb742ea6ad4e296153157fe364b26d2e2c4e4a3e
kavilac <kavilac.7@hotmail.com>
2016-10-19 17:56:00 -0500

    correcciones y comentarios
    correcciones y comentarios

--- Alignments.py
+++ Alignments.py
@@ -1,26 +1,71 @@
 import os
+import re
 from Bio import AlignIO
 from Bio import Phylo
 from Bio.Align.Applications import MuscleCommandline
 from Bio.Align.Applications import ClustalwCommandline
 
+"""Generates the aling file (*.aln) and the PhyloTree file (*.dnd) using ClustalW
+	INPUTS: input_file --> it's the name of a FASTA sequence(s) file.	Ex.:"opuntia.fasta"
+					path-->it's the ubication of ClustalW CommanLine Ex.:"/home/user/Desktop/clustalw2"""
 def clustal_align(input_file,path):
 	assert os.path.isfile(path), "Clustal W executable missing"
 	cline = ClustalwCommandline(path, infile=input_file)
 	cline()
-	
-def show_align(input_file):
-	align = get_align(input_file)
+
+"""Generates the aling file (*.aln) compatible with ClustalW using Muscle
+	INPUTS: input_file --> it's the name of a FASTA sequence(s) file.	Ex.:"opuntia.fasta"
+					output_file --> it's the file name used for the MULSCLE output with "aln" extension. 	Ex.:"opuntia.aln""""
+def muscle_align(input_file,output_file):
+	cline = MuscleCommandline(input=input_file, out=output_file,clw=True)
+	cline()
+
+"""Shows the synthesized aling conteined in the input file
+	INPUTS: input_file --> it's the name of a alignment file. Ex.: "opuntia.aln"
+					file_type --> it's an string refering to the file type. Ex.: "fasta""""
+def show_align(input_file,file_type):
+	align = get_align(input_file,file_type)
 	print(align)
-	
-def get_align(input_file):
-	align = AlignIO.read(input_file, "clustal")
+
+"""Shows the complite aling conteined in the input file 
+	INPUTS: input_file --> it's the name of a alignment file. Ex.: "opuntia.aln"
+					file_type --> it's an string refering to the file type. Ex.: "fasta""""
+def show_complite_align(input_file,file_type):
+	align = get_align(input_file,file_type)
+	for record in align:
+		print("%s - %s \n" % (record.seq, record.id))
+
+"""Gets the aling data form the input file
+	INPUTS: input_file --> it's the name of a alignment file. Ex.: "opuntia.aln"
+					file_type --> it's an string refering to the file type. Ex.: "fasta"""
+def get_align(input_file,file_type):
+	align = AlignIO.read(input_file, file_type)
 	return align
-		
+
+"""Shows the tree based in PhyloTree file (*.dnd) 
+	INPUTS: input_file --> it's the name of a PhyloTree file. Ex.: "opuntia.dnd""""
 def show_tree(input_file):
 	tree = Phylo.read(input_file, "newick")
 	Phylo.draw_ascii(tree)
-	
 
+"""Gets the Open Reading Frame of a sequence.
+	INPUTS: record_list --> it's a list of sequences (they usually are gotten by parse method)
+					type_seq --> it's an string that specifies the search data. 		Ex.: "gene"
+					ident --> it's an string that specifies the Open Reading Frame.		Ex.: "E6""""
+def get_ORF(record_list,type_seq,ident):
+	dicc = {type_seq : [ident]}
+	for record in record_list:
+		for feature in record.features:
+			if feature.qualifiers == dicc:
+				pos = str(feature.location)
+				match = re.search('(\d+):(\d+)', pos)
+				lower = int(match.group(1))
+				uper = int(match.group(2))
+				print("Locus: " + record.id + "\n" + str(feature.qualifiers) + "\tLimite inferior: " + str(lower) + "\tLimite superior: " + str(uper))
+				print(record[lower:uper].seq)
+				print("**********************************************************************")
+
+	
+	
 
 
--- Petitions.py
+++ Petitions.py
@@ -2,35 +2,59 @@ from Bio import Entrez
 from Bio import SeqIO
 import os
 
+"""Begins a NCBI connection using your e-mail address.
+	INPUTS: email --> e-mail address"""
 def init(email):
 	Entrez.email = email
-	
+
+"""Closes an open handle.
+	INPUTS: data --> an open handle"""
 def close(data):
 	data.close();
-	
-def search(data_base,term,email):
+
+"""Searches specific data into de NCBI database.
+	INPUTS: database  --> is an string that specifies the database where you want to search.		Ex.: "nucleotide"
+					term --> it's the name of the term that we want to search.		Ex.: "Human papillomavirus"
+					email --> it's your e-mail address for begining the NCBI connection
+	OUTPUTS: a list of sequence identifiers""""
+def search(database,term,email):
 	init(email)
-	handle = Entrez.esearch(db=data_base, term=term)
+	handle = Entrez.esearch(db=database, term=term)
 	record = Entrez.read(handle)
 	return record["IdList"]
-	
-def download(data_base,identifier,file_format,email):
+
+"""Downloads the data based on sequence identifiers
+	INPUTS: database  --> it's an string that specifies the database where you want to search.		Ex.: "nucleotide"
+					identifier --> it's a string of identifiers separated by commas Ex.: "LC027929,KC470291,FJ237042"
+					file_format --> it's a string that specifies the data format.
+					email --> it's your e-mail address for begining the NCBI connection
+	OUTPUTS: a handle object"""
+def download(database,identifier,file_format,email):
 	init(email)
-	handle = Entrez.efetch(db=data_base, id=identifier, rettype=file_format, retmode="text")
+	handle = Entrez.efetch(db=database, id=identifier, rettype=file_format, retmode="text")
 	return handle
 
+"""Parses a handle data.
+	INPUTS: data --> it's a handle object. (It usually gotten by the download method)
+					type_file --> it's a string that specifies the format of the handle data. 	Ex.:"genbank" 
+	OUTPUTS: list of parsed sequnce (SeqIO list)"""
 def parse(data,type_file):
 	record = list(SeqIO.parse(data, type_file))
 	return record
 
-def save(file_name,data):	
-	if not os.path.isfile(file_name):
-		out_handle = open(file_name, "w")
+"""Saves the handle data into a file.
+	INPUTS: filename --> it's the name which we want to save the file.
+					data --> it's a handle data """
+def save(filename,data):	
+	if not os.path.isfile(filename):
+		out_handle = open(filename, "w")
 		out_handle.write(data.read())
 		close(out_handle)
 		close(data)
 		print("Successfully saved")
 
+"""Shows some information of a sequences list like: identifier, sequence and size.
+	INPUTS: records_list --> a sequences list"""
 def show_info(records_list):
 	for i in range(len(records_list)):
 		node = records_list[i];
@@ -38,6 +62,9 @@ def show_info(records_list):
 		print("Secuencia: " + repr(node.seq))
 		print("Tam: " +str(len(node)) + "\n")
 
+"""Changes a list of identifier into a string of identifiers separated by commas
+	INPUTS: record_list -->a list of identifier
+	OUTPUTS: string of identifiers separated by commas """
 def format(records_list):
 	cadena  = ""
 	for i in range(len(records_list)):
